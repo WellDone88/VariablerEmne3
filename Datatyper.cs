91d3410cdf474111532484637f1dbb6b166b303f
WellDone88 <kenneth.moren@live.no>
2025-11-17 14:07:33 +0100

    sdasd

---
+++ Datatyper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+internal class Datatyper
+
+int Number = 5;
+string Word = "Hei";
+var SecondWord = "Hade";
+float desi = 6.66f;
+double desi2 = 6.66;
+decimal desi3 = 6.66M
+bool trueOrnot = true; 
+char letter = "a"
---
+++ VariablerEmne3/Datatyper.cs
@@ -0,0 +1,13 @@
+﻿internal class Datatyper
+{
+    int Number = 5;
+    string Word = "Hei";
+    char letter = 'a';
+
+    float desi = 6.66f;
+    double desi2 = 6.66;
+    decimal desi3 = 6.66M;
+    bool trueOrnot = true; 
+
+    
+}
